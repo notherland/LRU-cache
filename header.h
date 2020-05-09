42f53cc6735184f4e140808bb6afac575c85618f
notherland <55365450+notherland@users.noreply.github.com>
2020-05-10 01:39:20 +0400

    Create header.h

---
+++ header.h
@@ -0,0 +1,18 @@
+//
+// Created by Kostenok Elizaveta, Tsepkova Alexandra, Shirikova Ksenia on 10.05.2020.
+//
+
+#include <stdio.h>
+
+//hash table
+
+struct hash_table {
+    int capacity;
+    struct hash_elem*;
+};
+
+struct hash_table* Create_table();
+
+
+
+void Delete_table(struct hash_table* table);
