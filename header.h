ba54d31884f3e0d81e6fff2975d8ae03ad4b67b8
notherland <55365450+notherland@users.noreply.github.com>
2020-05-11 12:30:26 +0400

    Update header.h

--- header.h
+++ header.h
@@ -1,14 +1,45 @@
 //
-// Created by Kostenok Elizaveta, Tsepkova Alexandra, Shirikova Ksenia on 10.05.2020.
+// Created by Kostenok Elizaveta, Tsepkova Alexandra, Shirokova Ksenia on 10.05.2020.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
 
+struct page {
+    int index;
+    char* data;
+    int n; //кол-во символов
+};
+
+struct hash {
+    int capacity;
+    struct hash_node* elem;
+};
+
+struct queue
+{
+    int capacity;
+    int length;
+    struct node* head;
+    struct node* tail;
+};
+
 //hash table
 
-struct hash* Create_table();
+struct hash* create_table();
 
-struct hash_node* Check_index (int index, struct hash* table);
+void print_hash (struct hash* table);
 
-void Delete_table(struct hash* table);
+void delete_table(struct hash* table);
+
+//cache
+
+struct queue* queue_ctor();
+
+struct node* add_new_elem(struct queue* list, struct page* page, int* last_idx);
+
+void print_list(struct queue* s);
+
+void delete_queue(struct queue* list);
+
+struct node* move_elem(struct queue* list, struct node* cur_elem);
