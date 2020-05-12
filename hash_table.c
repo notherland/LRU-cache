c2f7689597a781505adaa909a789bfe0fba3ea49
notherland <55365450+notherland@users.noreply.github.com>
2020-05-13 01:34:58 +0400

    Update hash_table.c

--- hash_table.c
+++ hash_table.c
@@ -14,10 +14,25 @@ int hash_func (int index)
     return index % TABLESIZE;
 }
 
+
+struct hash *create_table(){
+    int i = 0;
+    struct hash *table = (struct hash *)calloc(1,sizeof(struct hash));
+    table->capacity = TABLESIZE;
+    table->elem = (struct hash_node**)calloc (1, sizeof (struct hash_node));
+    for (i = 0; i < table->capacity; i++)
+    {
+        table->elem[i] = (struct hash_node*) calloc (1, sizeof (struct hash_node));
+    }
+    return table;
+}
+
 struct hash_node* check_index (int index, struct hash* table)
 {
+    printf ("CHECK INDEX : %d\n", index);
     int key = hash_func(index);
-    struct hash_node* node = table[key].elem;
+    struct hash_node* node = (struct hash_node*)calloc (1, sizeof(struct hash_node));
+    node = table->elem[key];
     while (node != NULL)
     {
         if (index == node->index)
@@ -27,32 +42,25 @@ struct hash_node* check_index (int index, struct hash* table)
     return NULL;
 }
 
-struct hash *create_table(){
-    struct hash *table = (struct hash *)calloc(1,sizeof(struct hash));
-    table->capacity = TABLESIZE;
-    table->elem = (struct hash_node *)calloc(table->capacity, sizeof(struct hash_node));
-    for(int i = 0; i < table->capacity; i++){
-        table[i].elem = NULL;
-    }
-    return table;
-}
-
 struct node *add_new_page (struct page *page, struct hash *table, struct queue *list) {
-    struct hash_node *current = NULL, *check = NULL;
-    int last_idx = 0, i = 0, index;
+    struct hash_node *current = (struct hash_node *) calloc (1, sizeof(struct hash_node));
+    struct hash_node *check = (struct hash_node *) calloc (1, sizeof(struct hash_node));
+    int last_idx = 0, key = 0, index;
     struct node *newnode;
     index = page->index;
     check = check_index(index, table);
-    i = hash_func(index);
+    key = hash_func(index);
     if (check == NULL) { //такого элемента нет - создаем новый
         newnode = add_new_elem(list, page, &last_idx);
-        current = table[i].elem;
-        while (current != NULL) current = current->next;
+        current = table->elem[key];
+        printf ("%d", table->elem[key]);
+        while (current->next != NULL) current = current->next;
         current -> index = index;
         current -> n_cache = newnode;
         current -> next = NULL;
         if(last_idx != 0) {
-            //cюда функцию удаления ненужного адреса из хэша, передаем (last_idx, table);
+            printf ("hui");
+            //delete_hash_node (last_idx, table);
         }
         return newnode;
     }
@@ -67,7 +75,7 @@ void delete_table(struct hash* table){
     struct hash_node* list = NULL, *prev = NULL;
     for (i = 0; i < table->capacity; i += 1)
     {
-        list = table[i].elem;
+        list = table->elem[i];
         while (list != NULL)
         {
             prev = list;
@@ -81,12 +89,12 @@ void delete_table(struct hash* table){
 void print_hash (struct hash* table)
 {
     int i = 0;
-    struct hash_node* list = NULL;
+    struct hash_node* list = (struct hash_node*) calloc(1, sizeof(struct hash_node));
     printf ("hash-table:\n");
     for (i = 0; i < table->capacity; i+=1)
     {
         printf ("elem %d : ", i);
-        list = table->elem;
+        list = table->elem[i];
         while (list != NULL)
         {
             printf ("%d, ", list->index);
@@ -96,3 +104,4 @@ void print_hash (struct hash* table)
     }
     return;
 }
+
