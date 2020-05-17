f79f8173712aee8d817a78519d7d3192c16ed4a6
alexandra-tsepkova <tsepkova.ai@phystech.edu>
2020-05-17 21:58:54 +0300

    new hash_table

--- hash_table.c
+++ hash_table.c
@@ -1,6 +1,6 @@
 #include "LRU_cache.h"
 
-const int TABLESIZE = 1000;
+const int TABLESIZE = 1009;
 
 int hash_func(int index)
 {
@@ -9,29 +9,27 @@ int hash_func(int index)
 
 struct hash *create_table() {
     struct hash *table = (struct hash *)calloc(1, sizeof(struct hash));
-    assert (table);
     table->capacity = TABLESIZE;
-    table->elem = (struct hash_node **)calloc(table->capacity, sizeof(struct hash_node *));
-    assert (table->elem);
-
+    table->elem = (struct hash_node **)calloc(
+            table->capacity, sizeof(struct hash_node *));
     return table;
 }
 
 struct hash_node *check_index(int key, int index, struct hash *table) {
-    assert (table);
+    assert(table);
 
     struct hash_node *res;
     res = table->elem[key];
     while (res != NULL) {
-        if (index == (res->index))
+        if (index == (res->index)) {
             return res;
+        }
         res = res->next;
     }
-
     return NULL;
 }
 
-void remove_hash_node(struct hash_node *p) { //удаление элемента таблицы
+void remove_hash_node(struct hash_node *p) {
     assert (p);
 
     p->next = NULL;
@@ -39,71 +37,82 @@ void remove_hash_node(struct hash_node *p) { //удаление элемента
     free(p);
 }
 
-void delete_from_hash(int last_idx, struct hash *table) {
+struct hash_node *erase_hash_node(struct hash_node *p) {
+    assert(p);
+
+    p->next = NULL;
+    p->index = -666;
+    return p;
+}
+
+struct hash_node *delete_from_hash(int last_idx, struct hash *table) {
     assert (table);
 
     int idx = hash_func(last_idx);
     struct hash_node *p;
     struct hash_node *cur = table->elem[idx];
+
     if ((cur->index) == last_idx) {
         table->elem[idx] = cur->next;
-        remove_hash_node(cur);
-    } else {
-        while ((cur->next->index) != last_idx)
-            cur = cur->next;
-        p = cur->next;
-        cur->next = cur->next->next;
-        remove_hash_node(p);
+        return erase_hash_node(cur);
     }
+    while ((cur->next->index) != last_idx)
+        cur = cur->next;
+    p = cur->next;
+    cur->next = cur->next->next;
+
+    return erase_hash_node(p);
 }
 
 void add_new_page(struct hash *table, struct queue *list, int page, int *cache_hits) { //добавление в хэш
-    assert (table);
-    assert (list);
+    assert(table);
+    assert(list);
     assert (cache_hits);
 
     struct hash_node *newpage = NULL;
     struct hash_node *check = NULL;
     struct hash_node *p;
-    int last_idx = -1, key = 0;
-
+    int key = 0, last_idx = 0;
     key = hash_func(page);
     check = check_index(key, page, table);
-
-    if (check == NULL) { //такого элемента нет - создаем новый
-        newpage = (struct hash_node *)calloc(1, sizeof(struct hash_node));
+    if (check == NULL) {
+        if ((list->length) == (list->capacity)) {
+            last_idx = list->tail->data;
+            newpage = delete_from_hash(last_idx, table); //стираем последний элемент для записи нового
+        }
+        else
+            newpage = (struct hash_node *)calloc(1, sizeof(*newpage));
         p = table->elem[key];
         newpage->next = p;
         newpage->index = page;
         table->elem[key] = newpage;
-        newpage->n_cache = add_new_elem(list, page, &last_idx);
-        if (last_idx >= 0)
-            delete_from_hash(last_idx, table);
-    } else { //такой элемент есть, перемещаем его в кэше, хэш-таблицу не трогаем
+        newpage->n_cache = add_new_elem(list, page);
+    }
+    else {
         move_elem(list, check->n_cache);
         (*cache_hits)++;
     }
 }
 
 void print_hash(struct hash *table){
-    assert (table);
+    assert(table);
 
     int i = 0;
-    struct hash_node *list;
-    printf ("hash-table......\n");
-    for (i = 0; i < table->capacity; i += 1) {
-        printf ("elem %d : ", i);
+    struct hash_node *list = NULL;
+    printf ("hash-table....\n");
+    for (i = 0; i < (table->capacity); i++) {
+        printf ("elem %p : ", table->elem[i]);
         list = table->elem[i];
         while (list != NULL) {
-            printf ("index = %d, ", list->index);
+            printf ("%d ", list->index);
             list = list->next;
         }
         printf ("\n");
     }
 }
 
-void delete_table(struct hash *table) {
-    assert (table);
+void delete_table(struct hash *table) { //удаление хэш-таблицы
+    assert(table);
 
     int i = 0;
     struct hash_node *list = NULL, *prev = NULL;
@@ -115,4 +124,7 @@ void delete_table(struct hash *table) {
             remove_hash_node(prev);
         }
     }
+    free(table->elem);
+    free(table);
+    table = NULL;
 }
