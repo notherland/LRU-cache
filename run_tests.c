7312338c4b3643068d06f1aba0500d0f94471988
alexandra-tsepkova <tsepkova.ai@phystech.edu>
2020-05-18 01:46:18 +0300

    bugs fixed

--- run_tests.c
+++ run_tests.c
@@ -1,27 +1,27 @@
 #include "LRU_cache.h"
 
 void run_tests (){
-	run_test("test1.txt");
-	run_test("test2.txt");
-	run_test("test3.txt");
-	run_test("test4.txt");
-	run_test("test5.txt");
-	run_test("test6.txt");
-	run_test("test7.txt");
-	run_test("test8.txt");
-	run_test("test9.txt");
-	run_test("test10.txt");
-	run_test("test11.txt");
-	run_test("test12.txt");
-	run_test("test13.txt");
-	run_test("test14.txt");
-	run_test("test15.txt");
+	run_test("./tests/test1.txt");
+	run_test("./tests/test2.txt");
+	run_test("./tests/test3.txt");
+	run_test("./tests/test4.txt");
+	run_test("./tests/test5.txt");
+	run_test("./tests/test6.txt");
+	run_test("./tests/test7.txt");
+	run_test("./tests/test8.txt");
+	run_test("./tests/test9.txt");
+	run_test("./tests/test10.txt");
+	run_test("./tests/test11.txt");
+	run_test("./tests/test12.txt");
+	run_test("./tests/test13.txt");
+	run_test("./tests/test14.txt");
+	run_test("./tests/test15.txt");
 	return;
 }
 
 void run_test(const char *filename){
   
-  FILE *f = fopen("./tests/filename", "r");
+  FILE *f = fopen(filename, "r");
   int capacity = 0, n = 0, page = 0, cache_hits = 0, i = 0, answer;
   struct queue *my_cache;
   struct hash *my_hash;
@@ -36,9 +36,9 @@ void run_test(const char *filename){
     add_new_page(my_hash, my_cache, page, &cache_hits);
   }
   
-  fscanf(" %d", &answer);
+  fscanf(f, "%d", &answer);
   if(answer == cache_hits) printf("\ntest: result %d, correct %d \n OK\n", cache_hits, answer);
-  else printf("\n test: result %d, correct %d \nwrong answer\n");
+  else printf("\ntest: result %d, correct %d \nwrong answer\n", cache_hits, answer);
   
   delete_queue(my_cache);
   delete_table(my_hash);
